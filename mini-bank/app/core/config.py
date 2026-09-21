dc52a4cbb044465f5a0856a6c2b1e6094e49342b
andrewokala <ahndrhu25@gmail.com>
2026-09-21 11:17:19 +0100

    Implementation of API and Admin in progress...

---
+++ mini-bank/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    database_url: str
+    jwt_secret_token: str
+    jwt_access_token_expire_minutes: int = 15
+    redis_url=str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
